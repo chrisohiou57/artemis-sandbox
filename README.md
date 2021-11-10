74a2d523b242f07b4cc3528a0d546faf1cf6a61d
Chris Hannah <chrisohiou57@gmail.com>
2021-11-09 23:04:56 -0500

    Got a cluster deployment working. Added the prometheus pod annotations in the init script. May need to revisit how that's being done. Couldn't figure out how to do it with the CRD. Added a service for exposing the Hawtio console.

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ Build and push the custom broker image
 ########
 # NOTE #
 ########
-I'm not sure why I need to do this. When I use enableMetricsPlugin in the CRD and a custom init image I am able to configure everything except the prometheus metrics WAR file. It is not present in the default broker image. The only way I could get it to work is by extending the image to include the WAR file. The init image doesn't seem to allow us to copy WAR files. Also, I'm not sure how to add the Prometheus pod annoations with the CRD.
+I'm not sure why I need to do this. When I use enableMetricsPlugin in the CRD and a custom init image I am able to configure everything except the prometheus metrics WAR file. It is not present in the default broker image. The only way I could get it to work is by extending the image to include the WAR file. The init image doesn't seem to allow us to copy WAR files. Also, I'm not sure how to add the Prometheus pod annoations with the CRD. I ended up doing this with kubectl in the init image in my local for now.
 -->
 docker build -t chriaass/activemq-artemis-broker-kubernetes .\broker_image
 docker push chriaass/activemq-artemis-broker-kubernetes
@@ -64,10 +64,19 @@ This was ran from the artemis
 kubectl create -f artemis-basic-deployment.yaml
 kubectl delete -f artemis-basic-deployment.yaml
 
-<!-- Add annotations to tell Prometheus to scrape pod metrics -->
-kubectl annotate po ex-aao-ss-0 prometheus.io/scrape="true"
-kubectl annotate po ex-aao-ss-0 prometheus.io/port="8161"
-kubectl annotate po ex-aao-ss-0 prometheus.io/path="metrics"
+<!--
+Create a service to expose the console in one of the nodes in the cluster.
+This service is designed to expose the broker pod with the 0 index. The operator
+increments the number on the pod as brokers are added to the cluster. So, using
+the 0 index will work with a single broker or a cluster. If we spray traffic to all
+broker nodes we will get kicked out of the console as we bounce between pods and lose
+our session. I did test what happens if the 0 index pod goes down (I deleted it). The
+operator recreates the 0 index pod and you can login again once the pod is restored.
+
+TODO we may be able to get this working with all nodes with an Ingress. Couldn't get an Ingress working locally.
+TODO could also try configuring the service to use use SessionAffinity so we stick to the pod.
+-->
+kubectl apply -f artemis-console-service.yml
 
 <!-- Verify empty queue stats -->
 kubectl exec ex-aao-ss-0 -- /bin/bash /home/jboss/amq-broker/bin/artemis queue stat --user admin --password password --url tcp://ex-aao-ss-0:61616
--- operator/init_image/post-config.sh
+++ operator/init_image/post-config.sh
@@ -1,49 +1,34 @@
-# echo "RHEL version is..."
-# cat /etc/redhat-release
-# echo "Updating yum"
-# yum update
-
-# echo "#####################################################"
-# echo "##                NOTE: IMPORTANT!                 ##"
-# echo "## Installing EPEL outside of subscription-manager ##"
-# echo "## is not a recommended practice. This is just for ##"
-# echo "## demo purposes.                                  ##"
-# echo "#####################################################"
-
-# echo "Installing Extra Packages for Enterprise Linux (EPEL)"
-# yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
-
-# echo "Enabling the the optional, extras, and HA repositories"
-# subscription-manager repos --enable "rhel--optional-rpms" --enable "rhel--extras-rpms" --enable "rhel-ha-for-rhel-*-server-rpms"
-
-# echo "####################################################"
-# echo "##              Installing xmlstartlet            ##"
-# echo "####################################################"
-# yum install xmlstarlet -y
-
 echo "########## Custom Artemis Config Script ############"
-echo "##                                                ##"
 echo "##   Applying custom configuration to Artemis:    ##"
 echo "##       Artemis Prometheus Metrics Plugin        ##"
 echo "####################################################"
 
-echo "Copying artemis-prometheus-metrics-plugin.jar to $CONFIG_INSTANCE_DIR/lib"
+echo "Copying $HOSTNAME artemis-prometheus-metrics-plugin.jar to $CONFIG_INSTANCE_DIR/lib"
 cp /amq/artemis-prometheus-metrics-plugin.jar $CONFIG_INSTANCE_DIR/lib
 
-echo "Overwriting bootstrap.xml"
+echo "Overwriting $HOSTNAME bootstrap.xml with file that has metrics app definition"
 cp -f /amq/bootstrap.xml $CONFIG_INSTANCE_DIR/etc/bootstrap.xml
 
-# echo "Navigating to $CONFIG_INSTANCE_DIR/etc"
-# cd $CONFIG_INSTANCE_DIR/etc
+echo "Populating HOSTNAME:$HOSTNAME in bootstrap web binding address"
+sed -i 's/${HOSTNAME}/'"$HOSTNAME"'/g' $CONFIG_INSTANCE_DIR/etc/bootstrap.xml
 
-# echo "Adding metrics app to bootstrap.xml"
-# xmlstarlet edit --inplace -s /broker/web -t elem -n app -v "" -i /broker/web/app -t attr -n url -v metrics -i /broker/web/app -t attr -n war -v metrics.war bootstrap.xml
+echo "Copying kube config to $HOSTNAME"
+cp /amq/kube_config ~/.kube/config
 
-# xmlstarlet edit --inplace -s /broker/web -t elem -n AppTMP -v "" \
-#     -i //AppTMP -t attr -n "url" -v "metrics" \
-#     -i //AppTMP -t attr -n "war" -v "metrics.war" \
-#     -r //AppTMP -v app \
-#     bootstrap.xml
+echo "Adding yum repo for kubectl on $HOSTNAME and installing it"
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubectl
 
-# echo "Modified bootstrap.xml:"
-# cat bootstrap.xml
+echo "Found kube cluster info on $HOSTNAME: $(kubectl cluster-info)"
+echo "Adding Prometheus metric scraping annotations to $HOSTNAME in verbose mode"
+kubectl -v8 annotate po $HOSTNAME prometheus.io/scrape="true"
+kubectl -v8 annotate po $HOSTNAME prometheus.io/port="8161"
+kubectl -v8 annotate po $HOSTNAME prometheus.io/path="metrics"
